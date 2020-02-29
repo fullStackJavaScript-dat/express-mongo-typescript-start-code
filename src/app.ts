50f54b5f5dc25ebdc9c470546722755556f009a1
lars-m <lam@cphbusiness.dk>
2020-02-29 17:18:59 +0100

    new branch for the game project

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
 require('dotenv').config();
 import express from "express";
 import path from "path";
+import swaggerUi from "swagger-ui-express";
+
 const app = express();
 
+app.use(express.static(path.join(process.cwd(), 'public')))
+
+const openAPI_Documentation = require("./openApiDoc/spec.js");
+
+//You don't have to focus on this one
+app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(openAPI_Documentation));
 
 app.get("/api/dummy", (req, res) => {
   res.json({ msg: "Hello" })
